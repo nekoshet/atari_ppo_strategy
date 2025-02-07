d30183b98a039c8d5b05acd8a90dcf1a36b4c385
tal.levy <tal.levite@gmail.com>
2025-02-07 19:02:10 +0200

    fix display_observation_wrapper.py.

--- cleanrl/display_observation_wrapper.py
+++ cleanrl/display_observation_wrapper.py
@@ -1,4 +1,5 @@
 import gymnasium as gym
+from fontTools.unicodedata import block
 from gymnasium.core import ObsType, WrapperObsType
 from matplotlib import pyplot as plt
 
@@ -7,7 +8,13 @@ class DisplayObservation(gym.ObservationWrapper):
     def observation(self, observation: ObsType) -> WrapperObsType:
         fig, axes = plt.subplots(1, 3, figsize=(15, 5))
         for obs, ax in zip(observation, axes):
-            ax.matshow(obs)
+            ax.matshow(obs * 10)
         plt.tight_layout()
-        plt.show()
+        plt.show(block=True)
+        return observation
+
+    def observation2(self, observation: ObsType) -> WrapperObsType:
+        plt.matshow(observation * 10)
+        plt.tight_layout()
+        plt.show(block=True)
         return observation
