75cb498e427c76005bcb43900be8caeb7d4cc1a6
tal.levy <tal.levite@gmail.com>
2025-02-11 18:14:59 +0200

    adjust display_observation_wrapper.py for Alien.

--- cleanrl/display_observation_wrapper.py
+++ cleanrl/display_observation_wrapper.py
@@ -1,3 +1,5 @@
+import random
+
 import gymnasium as gym
 import numpy as np
 from gymnasium.core import ObsType, WrapperObsType
@@ -5,7 +7,7 @@ from matplotlib import pyplot as plt
 
 
 class DisplayObservation(gym.ObservationWrapper):
-    def observation(self, observation: ObsType) -> WrapperObsType:
+    def observation2(self, observation: ObsType) -> WrapperObsType:
         fig, axes = plt.subplots(1, 3, figsize=(15, 5))
         for obs, ax in zip(observation, axes):
             obs = np.concatenate([obs, np.zeros_like(obs[..., 0])[..., None]], axis=-1)
@@ -14,8 +16,8 @@ class DisplayObservation(gym.ObservationWrapper):
         plt.show(block=True)
         return observation
 
-    def observation2(self, observation: ObsType) -> WrapperObsType:
-        plt.matshow(observation * 10)
+    def observation(self, observation: ObsType) -> WrapperObsType:
+        plt.matshow(observation)
         plt.tight_layout()
         plt.show(block=True)
         return observation
