9912e3593f7e5429c48ab209c423a9e9da83a2cf
tal.levy <tal.levite@gmail.com>
2025-02-12 12:13:21 +0200

    disable frame skipping in Atari.

--- cleanrl/ppo_atari.py
+++ cleanrl/ppo_atari.py
@@ -102,14 +102,14 @@ def make_custom_base_env(env_id, **kwargs):
 def make_env(env_id, idx, capture_video, run_name):
     def make_atari_env():
         if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array")
+            env = gym.make(env_id, render_mode="rgb_array", frameskip=1)
             env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         else:
-            env = gym.make(env_id)
+            env = gym.make(env_id, frameskip=1)
         env = AlienPlayerFinder(env)
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env = NoopResetEnv(env, noop_max=30)
-        # env = MaxAndSkipEnv(env, skip=4)
+        env = MaxAndSkipEnv(env, skip=4)
         env = EpisodicLifeEnv(env)
         if "FIRE" in env.unwrapped.get_action_meanings():
             env = FireResetEnvFixed(env)
